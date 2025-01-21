de5ccbbf4114e6731ccdecc258bfc15e827ac1b5
Vedant Patil <vedantpatil02007@gmail.com>
2025-01-21 18:46:24 +0530

    commit today

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import for React 18
-import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-// Use createRoot for React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/saisamarth-web/', // Replace with your repo name
+  base: '/',
 });
